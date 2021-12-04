06424f372d9028537d051463fd5b0fe4c7c72eff
Shivani-techno <shivanichoudharychi@gmail.com>
2021-12-05 00:21:01 +0530

    Simon_Game

---
+++ simon_game.js
@@ -0,0 +1,9 @@
+
+var gamePattern=[];
+var buttonColors=["red","blue","grren","yellow"];
+
+function nextSequence(){
+  var randomnumber=Math.floor(Math.random()*4);
+};
+var randomChosenColour=buttonColors[randomnumber];
+gamePattern.push("randomChosenColour");
